c5ef7f3417e6a79a1c9b2a2eeb0a8056254ff30a
kiboma <omayioben@gmail.com>
2023-02-15 08:59:18 +0300

    Apdate created app with dynamic data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log("Hello Vue")
+const app=Vue.createApp({
+    data(){
+        return{
+            title:"Enemy of the people",
+            author:"Chinua Achebe",
+            age: 41
+        }
+
+    }
+})
+app.mount('#app')
